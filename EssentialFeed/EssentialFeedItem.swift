18e07c0b75c3352facccf7676f4e707d10857ac0
Vimal Das <vimaldasu@gmail.com>
2023-10-04 03:09:48 +0530

    Create EssentialFeed

---
+++ EssentialFeed/EssentialFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vimal Das on 04/10/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
