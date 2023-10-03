cb8c7dc9b16d102b5600c17ccc83508e403c728b
Vimal Das <vimaldasu@gmail.com>
2023-10-04 04:18:27 +0530

    Move "RemoteFeedLoader" component to production

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,49 +6,38 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
-
-class HTTPClientSpy: HTTPClient {
-    func get(from url: URL) {
-        requestedUrl = url
-    }
-    
-    var requestedUrl: URL?
-}
+import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesnotReuestDataFromUrl() {
-        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        _ = makeSUT()
         XCTAssertNil(client.requestedUrl)
     }
     
     func test_load_requestDataFromUrl() {
         let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         sut.load()
         
         XCTAssertEqual(client.requestedUrl, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedUrl: URL?
+        
+        func get(from url: URL) {
+            requestedUrl = url
+        }
     }
 }
