666e6b533d664b1be141620cb81deeba86a791f4
Vimal Das <vimaldasu@gmail.com>
2023-10-08 02:37:21 +0530

    separate date extension to sepreate

--- EssentialFeedTests/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Helpers/FeedCacheTestHelpers.swift
@@ -33,7 +33,9 @@ extension Date {
     func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
+}
     
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
     }
