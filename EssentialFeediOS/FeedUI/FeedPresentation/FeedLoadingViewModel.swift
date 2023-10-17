5f5ffd4f705c52065ed6cb38a97cc9853a8c544c
Vimal Das <vimaldasu@gmail.com>
2023-10-18 00:53:38 +0530

    Move ViewModels to separate files

---
+++ EssentialFeediOS/FeedUI/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Vimal Das on 18/10/23.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedUI/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Vimal Das on 18/10/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
